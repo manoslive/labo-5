672c01c0a36120bbe0257b8427535b44c4ed0063
manoslive <ebeloin@hotmail.com>
2014-11-20 11:32:47 -0500

    ajout calculerfibonaccirecursif

--- ExposantRecursif.cpp
+++ ExposantRecursif.cpp
@@ -11,5 +11,23 @@
 
 long ExposantRecursif::CalculerFibonacciRecursif(int n)
 {
+   int valeur = 0;
 
+   if (n <= 0)
+   {
+      return 0;
+   }
+
+   if (n == 1 || n == 2)
+   {
+      return 1;
+   }
+
+   if (valeur != 0)
+   {
+      return valeur;
+   }
+
+   valeur = CalculerFibonacciRecursif(n - 1) + CalculerFibonacciRecursif(n - 2);
+   return valeur;
 }
