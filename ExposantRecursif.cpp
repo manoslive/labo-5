6ba806d21c39e7c226bfe66d9f07c56a7df2ef95
manoslive <ebeloin@hotmail.com>
2014-11-20 11:41:05 -0500

    5i56ikt

--- ExposantRecursif.cpp
+++ ExposantRecursif.cpp
@@ -1,10 +1,10 @@
 //----------------------------------------------------------------------------//
-// Labo 5 - Liste de Fibonacci    											//
-//	Shaun Cooper & Emmanuel Beloin										    //
-//	Créé le : 19-11-14													    //
-//	Dernière modification : 19-11-14									        //
-//	ExposantRecursif.cpp													//
-//	Déclarations des méthodes d'un ExposantRecursif							//
+// Labo 5 - Liste de Fibonacci    									            		//
+//	Shaun Cooper & Emmanuel Beloin								            		   //
+//	Créé le : 19-11-14												               	   //
+//	Dernière modification : 19-11-14								            	      //
+//	ExposantRecursif.cpp											                  		//
+//	Déclarations des méthodes d'un ExposantRecursif			         				//
 //----------------------------------------------------------------------------//
 
 #include "ExposantRecursif.h"
@@ -12,22 +12,23 @@
 long ExposantRecursif::CalculerFibonacciRecursif(int n)
 {
    int valeur = 0;
-
-   if (n <= 0)
+   if (n >= 3)
    {
-      return 0;
-   }
+      if (n <= 0)
+      {
+         return 0;
+      }
 
-   if (n == 1 || n == 2)
-   {
-      return 1;
-   }
+      if (n == 1 || n == 2)
+      {
+         return 1;
+      }
 
-   if (valeur != 0)
-   {
-      return valeur;
+      if (valeur != 0)
+      {
+         return valeur;
+      }
    }
-
    valeur = CalculerFibonacciRecursif(n - 1) + CalculerFibonacciRecursif(n - 2);
    return valeur;
 }
